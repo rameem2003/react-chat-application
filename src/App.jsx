f33cf1584e21fb3c9597b54ffbd8558498a7612b
rameem2003 <rameem2019@gmail.com>
2023-09-29 09:08:05 +0600

    remove some line in app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useContext, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
